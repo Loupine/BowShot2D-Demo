6dd10b2aee3edfed4f85bf5605ece0d733212994
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-09-05 18:43:27 -0400

    Make the arrow only shoot once and change arrow's power, gravity scale, and mass.

--- project/Bow/BowAndArrow.gd
+++ project/Bow/BowAndArrow.gd
@@ -1,13 +1,14 @@
 extends RigidBody2D
 
+var shot = false
 
 func _process(_delta):
-	
-	if Input.is_action_pressed("shoot"):
+	if Input.is_action_pressed("shoot") and not shot:
 		prepare_arrow()
-		var impulse = Vector2(100, 0).rotated(deg2rad($BowSprite.rotation_degrees))
+		var impulse = Vector2(300, 0).rotated(deg2rad($BowSprite.rotation_degrees))
 		$Arrow.apply_impulse(Vector2.ZERO, impulse)
 		$BowSprite.set_texture(preload("res://Bow/Bow.png"))
+		shot = true
 		
 	elif Input.is_action_pressed("aim_higher"):
 		$BowSprite.rotation_degrees -= 1
@@ -23,6 +24,6 @@ func prepare_arrow():
 	#makes sure the arrow is fired in the right orientation, is visible, 
 	#has colision, and has gravity before firing
 	$Arrow/ArrowSprite.rotation_degrees = $BowSprite.rotation_degrees
-	$Arrow.gravity_scale = 5
+	$Arrow.gravity_scale = 1
 	$Arrow.visible = true
 	$Arrow/ArrowCollision.disabled = false
