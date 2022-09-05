5e614a39319b534a105f5c7345bff577e4f01323
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-09-05 11:39:06 -0400

    Add prepare_arrow() method to refactor code under 'shoot' input check.

--- project/Bow/BowAndArrow.gd
+++ project/Bow/BowAndArrow.gd
@@ -4,12 +4,9 @@ extends RigidBody2D
 func _process(_delta):
 	
 	if Input.is_action_pressed("shoot"):
-		$Arrow/ArrowSprite.rotation_degrees = $BowSprite.rotation_degrees
-		$Arrow.visible = true
-		$Arrow.gravity_scale = 5
+		prepare_arrow()
 		$Arrow.apply_impulse(Vector2.ZERO, Vector2(100, 0).rotated(deg2rad($BowSprite.rotation_degrees)))
-		$Arrow/CollisionShape2D.disabled = false
-	
+		
 	elif Input.is_action_pressed("aim_higher"):
 		$BowSprite.rotation_degrees -= 1
 		print($BowSprite.rotation_degrees)
@@ -19,3 +16,10 @@ func _process(_delta):
 		print($BowSprite.rotation_degrees)
 	
 	$BowSprite.rotation_degrees = clamp($BowSprite.rotation_degrees, -90, 0)
+
+func prepare_arrow():
+	#makes sure the arrow is fired in the right orientation, is visible, has colision, and has gravity
+	$Arrow/ArrowSprite.rotation_degrees = $BowSprite.rotation_degrees
+	$Arrow.gravity_scale = 5
+	$Arrow.visible = true
+	$Arrow/ArrowCollision.disabled = false
