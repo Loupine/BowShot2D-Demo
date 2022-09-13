e6b83fa2bc0993974bd10bc2dba1e1d0010cec88
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-09-12 20:31:22 -0400

    Separate the Bow and Arrow from the BowAndArrow scene and move controls to World.gd
    The arrow is now a separate projectile, and all manipulations are currently done in the World script.

--- project/World/World.gd
+++ project/World/World.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends Node2D
 
 var shot = false
 var power = 200
@@ -7,18 +7,18 @@ func _process(_delta):
 	if not shot:
 		if Input.is_action_pressed("shoot"):
 			prepare_arrow()
-			var impulse = Vector2(power, 0).rotated(deg2rad($BowSprite.rotation_degrees))
+			var impulse = Vector2(power, 0).rotated(deg2rad($Bow/BowSprite.rotation_degrees))
 			$Arrow.apply_impulse(Vector2.ZERO, impulse)
-			$BowSprite.set_texture(preload("res://Bow/Bow.png"))
+			$Bow/BowSprite.set_texture(preload("res://Bow/Bow.png"))
 			shot = true
 		
 		elif Input.is_action_pressed("aim_higher"):
-			$BowSprite.rotation_degrees -= 1
-			print('Angle: ', round(-$BowSprite.rotation_degrees))
+			$Bow/BowSprite.rotation_degrees -= 1
+			print('Angle: ', round(-$Bow/BowSprite.rotation_degrees))
 		
 		elif Input.is_action_pressed("aim_lower"):
-			$BowSprite.rotation_degrees += 1
-			print('Angle: ', round(-$BowSprite.rotation_degrees))
+			$Bow/BowSprite.rotation_degrees += 1
+			print('Angle: ', round(-$Bow/BowSprite.rotation_degrees))
 		
 		elif Input.is_action_pressed("increase_power"):
 			power += 10
@@ -33,12 +33,12 @@ func _process(_delta):
 		print(get_tree().reload_current_scene())
 	
 	power = clamp(power, 10, 590)
-	$BowSprite.rotation_degrees = clamp($BowSprite.rotation_degrees, -89, -1)
+	$Bow/BowSprite.rotation_degrees = clamp($Bow/BowSprite.rotation_degrees, -89, -1)
 
 func prepare_arrow():
 	#makes sure the arrow is fired in the right orientation, is visible, 
 	#has colision, and has gravity before firing
-	$Arrow/ArrowSprite.rotation_degrees = $BowSprite.rotation_degrees
+	$Arrow/ArrowSprite.rotation_degrees = $Bow/BowSprite.rotation_degrees
 	$Arrow.gravity_scale = 1
 	$Arrow.visible = true
 	$Arrow/ArrowCollision.disabled = false
