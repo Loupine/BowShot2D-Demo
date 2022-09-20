0e21bde5ced161566c54c6f9044f28731716133b
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-09-19 23:45:24 -0400

    Animate labels when values change.

--- project/World/HUD.gd
+++ project/World/HUD.gd
@@ -10,13 +10,16 @@ func _on_World_power_changed(new_power):
 	set_power_text(new_power)
 
 func set_angle_text(angle):
+	$HUDAnimationPlayer.play("GrowAngleLabel")
 	$VBoxContainer/AngleLabel.text = "Angle: " + str(round(angle))
 
 func set_inventory_size_text(arrows_left):
+	$HUDAnimationPlayer.play("GrowInventoryLabel")
 	$VBoxContainer/InventorySizeLabel.text = "Arrows left: " + str(arrows_left)
 	
 	if arrows_left == 0:
 		$VBoxContainer/InventorySizeLabel.text = "No arrows left! Game Over!\nPress Reload to reset the game."
 
 func set_power_text(power):
+	$HUDAnimationPlayer.play("GrowPowerLabel")
 	$VBoxContainer/PowerLabel.text = "Power: " + str(power)
