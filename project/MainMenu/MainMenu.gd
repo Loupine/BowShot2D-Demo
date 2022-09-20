889342f92a1da492aeb1380b84eeffb3a915b771
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-09-19 21:59:34 -0400

    Fix an unused value warning when changing scenes.

--- project/MainMenu/MainMenu.gd
+++ project/MainMenu/MainMenu.gd
@@ -1,4 +1,5 @@
 extends Control
 
 func _on_PlayButton_pressed():
-	get_tree().change_scene_to(preload("res://World/World.tscn"))
+	# print out the return value to fix the unused value warning
+	print(get_tree().change_scene_to(preload("res://World/World.tscn")))
